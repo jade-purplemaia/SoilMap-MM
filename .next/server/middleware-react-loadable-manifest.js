a0dcfe1307db9ce5a7c1c20d652c974ec9860a24
Leimomi <leimomibong@gmail.com>
2025-07-16 08:59:50 -0700

    added dependencies to get the app running locally

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST="{\"pages/index.js -> ../components/SoilMap\":{\"id\":\"pages/index.js -> ../components/SoilMap\",\"files\":[\"static/chunks/src_components_SoilMap_js.js\"]}}"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import dynamic from 'next/dynamic';
 import Sidebar from '../components/Sidebar';
-import styles from '../styles/globals.css';
 
 const SoilMap = dynamic(() => import('../components/SoilMap'), { ssr: false });
 
